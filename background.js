efe6d40767e03e9e32280cac793586ba7fdad3a1
Spencer <spencer@slickremix.com>
2023-04-28 00:56:30 -0400

    0.0.1

---
+++ background.js
@@ -0,0 +1,81 @@
+async function fetchApiKey() {
+    const { apiKey } = await chrome.storage.sync.get("apiKey");
+    return apiKey;
+}
+
+// This is the proper URL to use.
+// https://api.openai.com/v1/engines/text-davinci-002/completions
+async function getSuggestionsFromApi(apiKey, prompt, maxTokens, n, stop, temperature, engine) {
+    if (!apiKey) {
+        throw new Error("API key not provided. Please enter your API key in the settings.");
+    }
+
+    // gpt3: curie
+    // gpt4: text-davinci-002
+    const engineURL = engine === 'gpt3' ? 'https://api.openai.com/v1/engines/curie/completions' : 'https://api.openai.com/v1/engines/text-davinci-002/completions';
+
+    console.log( engineURL );
+
+    const response = await fetch(engineURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            prompt: prompt,
+            max_tokens: parseInt(maxTokens) || 250,
+            n: parseInt(n) || 1,
+            stop: stop || null,
+            temperature: parseFloat(temperature) || 0.1
+        })
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        if (errorData.error && errorData.error.code === "invalid_api_key") {
+            throw new Error("Invalid API key provided. Please check your API key.");
+        } else {
+            throw new Error("Unexpected response from OpenAI API: " + JSON.stringify(errorData));
+        }
+    }
+
+    const data = await response.json();
+    if (data.choices) {
+        return data.choices.map((choice) => choice.text.trim());
+    } else {
+        console.error("Unexpected response from OpenAI API:", JSON.stringify(data, null, 2));
+        return [];
+    }
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "getSuggestions") {
+        if (request.text) {
+            fetchApiKey().then(async function (apiKey) {
+                const settings = await chrome.storage.sync.get(["prompt", "maxTokens", "n", "stop", "temperature", "engine"]);
+                try {
+                    const suggestions = await getSuggestionsFromApi(
+                        apiKey,
+                        request.text,
+                        settings.maxTokens || 50,
+                        settings.n || 1,
+                        settings.stop || null,
+                        settings.temperature || 0.1,
+                        settings.engine || 'gpt4'
+                    );
+                    sendResponse({ suggestions: suggestions });
+                } catch (error) {
+                    console.error('Error:', error);
+                    sendResponse({ error: error.message });
+                }
+            }).catch(function (error) {
+                console.error("Error fetching API key or settings:", error);
+            });
+            return true; // Required for async sendResponse
+        } else {
+            console.error("No text provided.");
+        }
+    }
+});
+
---
+++ layout/js/popup.js
@@ -0,0 +1,350 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Initialize Materialize tooltips
+    const tooltips = document.querySelectorAll(".tooltipped");
+    M.Tooltip.init(tooltips);
+
+    // Add the event listener for the dark mode toggle switch
+    document.getElementById("darkModeToggle").addEventListener("change", function() {
+        document.body.classList.toggle("dark-mode", this.checked);
+    });
+
+
+    const inputText = document.getElementById("input-text");
+
+    inputText.addEventListener("input", function () {
+        chrome.storage.local.set({ savedText: this.value }, function () {
+            console.log("Text saved.");
+        });
+    });
+
+    document.getElementById("engineToggle").addEventListener("change", function() {
+        chrome.storage.sync.set({ engine: this.checked ? 'gpt3' : 'gpt4' }, function() {
+            console.log("Engine setting saved.");
+        });
+    });
+
+    chrome.storage.local.get("savedText", function (data) {
+        inputText.value = data.savedText || "";
+    });
+
+    // Add event listeners for the tablinks
+    document.querySelectorAll(".tablinks").forEach((tab) => {
+        tab.addEventListener("click", function (event) {
+            openTab(event.target.getAttribute("data-tab"));
+            // Remove active class from other tablinks
+            document.querySelectorAll(".tablinks").forEach(t => t.classList.remove("active"));
+            event.target.classList.add("active");
+        });
+    });
+
+    // Open the Suggestions tab by default
+    openTab("suggestions-tab");
+    document.querySelector('[data-tab="suggestions-tab"]').classList.add("active");
+
+    var inputs = document.querySelectorAll('input');
+
+    inputs.forEach(function(input) {
+        input.addEventListener('focus', function() {
+            var label = document.querySelector('label[for="' + input.id + '"]');
+            if (label) {
+                label.style.color = '#1a73e8'; // Change this to the desired color
+            }
+        });
+
+        input.addEventListener('blur', function() {
+            var label = document.querySelector('label[for="' + input.id + '"]');
+            if (label) {
+                label.style.color = ''; // Reset the color to the default
+            }
+        });
+    });
+
+    // Add a cancel flag
+    let cancelRequest = false;
+
+    const submitButton = document.getElementById("submit-button");
+    if (submitButton) {
+        submitButton.addEventListener("click", async () => {
+            if (submitButton.innerText === "Get Suggestions") {
+
+                const inputText = document.getElementById("input-text").value;
+                if (inputText.trim() === "") {
+                    displaySuggestions(["Add some text above to get a response."]);
+                    return;
+                }
+
+                try {
+                    const inputText = document.getElementById("input-text").value;
+                    showLoadingAnimation();
+                    submitButton.innerText = "Cancel";
+                    cancelRequest = false;
+                    const engine = document.getElementById("engineToggle").checked ? 'gpt3' : 'gpt4';
+                    const response = await sendMessageToBackground(inputText, { action: "getSuggestions", engine });
+                    if (!cancelRequest) {
+                        const suggestions = response.suggestions || [];
+                        displaySuggestions(suggestions);
+                    }
+                } catch (error) {
+                        console.error("Error:", error.message);
+                        displaySuggestions([`Error: ${error.message}`]);
+                } finally {
+                    hideLoadingAnimation();
+                    submitButton.innerText = "Get Suggestions";
+                }
+            } else {
+                cancelRequest = true;
+                submitButton.innerText = "Cancel";
+            }
+        });
+    } else {
+        console.error('submit-button not found');
+    }
+
+
+
+    const clearButton = document.getElementById("clear-button");
+    if (clearButton) {
+        clearButton.addEventListener("click", () => {
+            // Clear the suggestions
+            displaySuggestions([]);
+
+            // Clear the input field
+            const inputText = document.getElementById("input-text");
+            inputText.value = "";
+
+            // Clear the savedText in chrome.storage.local
+            chrome.storage.local.set({ savedText: "" }, function () {
+                console.log("Text cleared.");
+            });
+        });
+    } else {
+        console.error('clear-button not found');
+    }
+});
+
+(async () => {
+    // Wrap the event listener assignment in a DOMContentLoaded event listener
+    document.addEventListener("DOMContentLoaded", () => {
+        const settingsForm = document.getElementById("settings-form");
+        if (settingsForm) {
+            settingsForm.addEventListener("submit", (e) => {
+                e.preventDefault();
+
+                // Show the "Saving" overlay
+                const savingOverlay = showSavingOverlay();
+
+                // Save the settings here, for example, using the chrome.storage API
+                const apiKey = document.getElementById("api-key").value;
+                const maxTokens = document.getElementById("max-tokens").value;
+                const n = document.getElementById("n").value;
+                const stop = document.getElementById("stop").value;
+                const temperature = document.getElementById("temperature").value;
+                const darkMode = document.getElementById("darkModeToggle").checked;
+
+                chrome.storage.sync.set({
+                    apiKey: apiKey,
+                    maxTokens: maxTokens,
+                    n: n,
+                    stop: stop,
+                    temperature: temperature,
+                    darkMode: darkMode
+                }, () => {
+                    console.log('Settings saved');
+
+                    // Remove the "Saving" overlay after a short delay
+                    setTimeout(() => {
+                        savingOverlay.remove();
+                    }, 1000);
+                });
+            });
+        } else {
+            console.error('settings-form not found');
+        }
+    });
+
+
+    // Load the settings when the popup is opened, for example, using the chrome.storage API
+    chrome.storage.sync.get(['apiKey', 'maxTokens', 'n', 'stop', 'temperature', 'darkMode', 'engine'], (result) => {
+        document.getElementById("api-key").value = result.apiKey || '';
+        document.getElementById("max-tokens").value = result.maxTokens || '';
+        document.getElementById("n").value = result.n || '';
+        document.getElementById("stop").value = result.stop || '';
+        document.getElementById("temperature").value = result.temperature || '';
+        document.getElementById("darkModeToggle").checked = result.darkMode || false;
+        document.body.classList.toggle("dark-mode", result.darkMode);
+        document.getElementById("engineToggle").checked = result.engine === 'gpt3';
+    });
+})();
+
+chrome.storage.local.get("savedSuggestions", function (data) {
+    const suggestions = data.savedSuggestions || [];
+    displaySuggestions(suggestions);
+});
+
+
+// Add this function to copy text to the clipboard
+function copyToClipboard(text) {
+    const el = document.createElement('textarea');
+    el.value = text;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
+
+function showCopiedOverlay(target) {
+    const overlay = document.createElement('div');
+    overlay.classList.add('copied-overlay');
+    overlay.textContent = 'Copied';
+
+    // Set position and size of the overlay
+    const suggestionRect = target.getBoundingClientRect();
+
+    const containerRect = target.parentElement.getBoundingClientRect();
+
+    overlay.style.position = 'absolute';
+    overlay.style.top = (suggestionRect.top - containerRect.top) + 'px';
+    overlay.style.left = (suggestionRect.left - containerRect.left) + 'px';
+    overlay.style.width = suggestionRect.width + 'px';
+    overlay.style.height = suggestionRect.height + 'px';
+    overlay.style.display = 'flex';
+    overlay.style.justifyContent = 'center';
+    overlay.style.alignItems = 'center';
+
+    document.getElementById("suggestions-container").appendChild(overlay);
+    setTimeout(() => {
+        document.getElementById("suggestions-container").removeChild(overlay);
+    }, 1500);
+}
+
+function openTab(tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+}
+
+document.querySelectorAll(".tablinks").forEach((tab) => {
+    tab.addEventListener("click", function (event) {
+        openTab(event.target.getAttribute("data-tab"));
+    });
+});
+
+function sendMessageToBackground(text, message, cancelSignal) {
+    return new Promise((resolve, reject) => {
+        const listener = function (response) {
+            if (chrome.runtime.lastError) {
+                reject(chrome.runtime.lastError);
+            } else if (response.error) {
+                reject(new Error(response.error));
+            } else {
+                resolve(response);
+            }
+        };
+
+        chrome.runtime.sendMessage({ ...message, text }, listener);
+
+        if (cancelSignal) {
+            cancelSignal.addEventListener('cancel', () => {
+                chrome.runtime.onMessage.removeListener(listener);
+                reject(new Error('Request canceled'));
+            });
+        }
+    });
+}
+
+
+function createSuggestionElement(text) {
+    const suggestion = document.createElement("div");
+    suggestion.classList.add("suggestion");
+    suggestion.textContent = text;
+
+    // Add an event listener to the suggestion element
+    suggestion.addEventListener('click', (e) => {
+        copyToClipboard(text);
+        showCopiedOverlay(e.target);
+    });
+
+    return suggestion;
+}
+
+function displaySuggestions(suggestions) {
+    const suggestionsContainer = document.getElementById("suggestions-container");
+    suggestionsContainer.innerHTML = "";
+
+    for (const suggestion of suggestions) {
+        const suggestionElement = createSuggestionElement(suggestion);
+        suggestionsContainer.appendChild(suggestionElement);
+    }
+
+    // Save the suggestions to local storage
+    chrome.storage.local.set({ savedSuggestions: suggestions }, function () {
+        console.log("Suggestions saved.");
+    });
+
+    // Show or hide the "Clear" button
+    const clearButton = document.getElementById("clear-button");
+    if (clearButton) {
+        if (suggestions.length > 0) {
+            clearButton.style.display = "inline-block";
+        } else {
+            clearButton.style.display = "none";
+        }
+    }
+}
+
+
+function showLoadingAnimation() {
+    const suggestionsContainer = document.getElementById("suggestions-container");
+    suggestionsContainer.innerHTML = ""; // Clear the suggestions container
+
+    const loadingAnimation = document.createElement("div");
+    loadingAnimation.classList.add("loading-animation");
+
+    for (let i = 0; i < 3; i++) {
+        const dot = document.createElement("div");
+        dot.classList.add("dot");
+        loadingAnimation.appendChild(dot);
+    }
+
+    suggestionsContainer.appendChild(loadingAnimation);
+}
+
+function hideLoadingAnimation() {
+    const loadingAnimation = document.querySelector(".loading-animation");
+    if (loadingAnimation) {
+        document.getElementById("suggestions-container").removeChild(loadingAnimation);
+    }
+}
+
+function showSavingOverlay() {
+    const settingsContent = document.getElementById("settings-tab");
+    const savingOverlay = document.createElement("div");
+    savingOverlay.classList.add("saving-overlay");
+    savingOverlay.textContent = "Saving...";
+
+    savingOverlay.style.position = "absolute";
+    savingOverlay.style.top = "0";
+    savingOverlay.style.left = "0";
+    savingOverlay.style.width = "100%";
+    savingOverlay.style.height = "100%";
+    savingOverlay.style.display = "flex";
+    savingOverlay.style.justifyContent = "center";
+    savingOverlay.style.alignItems = "center";
+    savingOverlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    savingOverlay.style.color = "white";
+    savingOverlay.style.zIndex = "10";
+
+    settingsContent.appendChild(savingOverlay);
+
+    return savingOverlay;
+}
